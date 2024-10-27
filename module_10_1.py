4f9163be5006cb43d8b2110872ed7af92dc17a1e
VitoBJ <Shizane7@gmail.com>
2024-10-27 23:40:32 +0300

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,33 @@
+import threading
+from time import sleep
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+
+threads = []
+thread_arguments = [
+    (10, 'example5.txt'),
+    (30, 'example6.txt'),
+    (200, 'example7.txt'),
+    (100, 'example8.txt')
+]
+
+for args in thread_arguments:
+    thread = threading.Thread(target=write_words, args=args)
+    threads.append(thread)
+    thread.start()
+
+
+for thread in threads:
+    thread.join()
